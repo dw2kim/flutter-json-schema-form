925734e4b080d176ff35087631acb2178c1bca41
daewon <dw2kim@gmail.com>
2019-12-24 14:54:31 -0500

    After compile

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/daewonkim/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/daewonkim/Repository/flutter-json-schema-form"
+export "FLUTTER_TARGET=/Users/daewonkim/Repository/flutter-json-schema-form/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/daewonkim/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
